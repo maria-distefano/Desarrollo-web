5fa6b66ca7efb33a39d6a21e0f617dc51d0eca16
María Di Stefano <marias.distefano@gmail.com>
2022-06-09 22:03:52 -0300

    con sass

---
+++ script.js
@@ -0,0 +1,31 @@
+document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
+
+document.getElementById("back_menu").addEventListener("click", ocultar_menu);
+
+nav = document.getElementById("nav");
+background_menu = document.getElementById("back_menu");
+
+function mostrar_menu(){
+
+    nav.style.right = "0px";
+    background_menu.style.display = "block";
+}
+
+function ocultar_menu(){
+
+    nav.style.right = "-250px";
+    background_menu.style.display = "none";
+}
+
+
+/*------------formulario-------------*/
+var inputs = document.getElementsByClassName('formulario__input');
+for (var i =0; i < inputs.length; i++){
+    inputs[i].addEventListener('keyup', function(){
+        if(this.value.length>=1){
+            this.nextElementSibling.classList.add('fijar');
+        }else{
+            this.nextElementSibling.classList.remove('fijar');
+        }
+    });
+}
